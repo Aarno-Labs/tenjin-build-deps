6699ea154ac552d31c25f7b6227f67e2c0645feb
Ben Karel <bkarel@aarno-labs.com>
2025-07-09 02:10:56 -0400

    Give TMPSUBDIR a path that includes Tenjin, in case it leaks

--- thirdparty/common-vars.sh
+++ thirdparty/common-vars.sh
@@ -1,5 +1,5 @@
 BUILDERNAME="${BUILDER:-debian-bullseye}"
 IMAGENAME="ghcr.io/aarno-labs/tenjin-${BUILDERNAME}-builder:rev-85f982358"
-TMPSUBDIR=/tmp/183018384101018888
+TMPSUBDIR=/tmp/tenjin-build-deps/canary/183018384101018888
 
 OUTDIR=$(realpath $SCRIPTDIR/../out)
