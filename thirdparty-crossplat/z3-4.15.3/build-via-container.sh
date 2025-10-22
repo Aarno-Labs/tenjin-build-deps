a67042e56c800d3a91842d9b1325c11f8f9e1b6d
Ben Karel <bkarel@aarno-labs.com>
2025-10-22 10:02:49 -0400

    Limit z3 build to 3-way parallelism
    The `ubuntu-24.04-arm` runner on GitHub seems to be failing part way
    through the build, maybe it's hitting an OOM killer?

--- thirdparty-crossplat/z3-4.15.3/build-via-container.sh
+++ thirdparty-crossplat/z3-4.15.3/build-via-container.sh
@@ -22,7 +22,7 @@ docker run --rm -i -v $SCRIPTDIR:/inputs -v $OUTDIR:/outputs \
   apt-get update && apt-get -y --no-install-recommends install
 
   cmake -B build -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/outputs
-  cmake --build   build --parallel
+  cmake --build   build -j 3
   cmake --install build
 
   chown -R $(id -u):$(id -g) /outputs
