0307bf5c2a6f63693baf5e7de72ad9bdad763d6a
Ben Karel <bkarel@aarno-labs.com>
2025-10-22 01:10:19 -0400

    Initial infrastructure for build deps on Mac too
    So far all our build deps have been for things we need on Linux
    but are already provided by the system toolchain on Mac. But soon
    we will want some cross-platform dependencies. First up is Ninja,
    mostly as a simple and self-contained patient zero.

---
+++ thirdparty-crossplat/ninja-1.13.1/build-via-container.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -eux
+
+OS_NAME=$(uname -s)
+case "${OS_NAME}" in
+    Linux*)
+################################################################################
+SCRIPTDIR=$(dirname $(realpath "$0"))
+source $SCRIPTDIR/../../thirdparty-linux/common-vars.sh
+
+docker run --rm -i -v $SCRIPTDIR:/inputs -v $OUTDIR:/outputs \
+            --network=host \
+                     $IMAGENAME    sh -s <<EOF
+  set -eux
+  mkdir /tmp/work
+  cd /tmp/work
+
+  tar xf /inputs/*.tar.*
+  cd ninja-*/
+
+  cmake -B build -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/outputs -DBUILD_TESTING=OFF
+  cmake --build   build -- -j$(nproc --all)
+  cp build/ninja /outputs/bin/ninja
+
+  chown -R $(id -u):$(id -g) /outputs
+EOF
+################################################################################
+        ;;
+
+    Darwin*)
+################################################################################
+  TMPDIR=/tmp/xj-ninja
+  rm -rf $TMPDIR
+  mkdir $TMPDIR
+  cd $TMPDIR 
+
+  tar xf $SCRIPTDIR/*.tar.*
+  cd ninja-*/
+
+  cmake -B build -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+  cmake --build   build -- -j$(nproc --all)
+  mkdir -p       $OUTDIR/bin
+  cp build/ninja $OUTDIR/bin/ninja
+
+  cd ..
+  rm -rf $TMPDIR
+################################################################################
+        ;;
+
+    *)
+        echo "Running on an unknown OS: ${OS_NAME}"
+        # Handle other operating systems or provide a default action
+        ;;
+esac
+
