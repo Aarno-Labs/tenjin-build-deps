c92b87d8f1e16f0af40c7184889685f042d8743f
Ben Karel <bkarel@aarno-labs.com>
2025-10-22 02:13:07 -0400

    Have z3 build on Mac too

--- thirdparty-crossplat/z3-4.15.3/build-via-container.sh
+++ thirdparty-crossplat/z3-4.15.3/build-via-container.sh
@@ -3,8 +3,12 @@
 set -eux
 
 SCRIPTDIR=$(dirname $(realpath "$0"))
-source $SCRIPTDIR/../common-vars.sh
+source $SCRIPTDIR/../../thirdparty-linux/common-vars.sh
 
+OS_NAME=$(uname -s)
+case "${OS_NAME}" in
+    Linux*)
+################################################################################
 docker run --rm -i -v $SCRIPTDIR:/inputs -v $OUTDIR:/outputs \
             --network=host \
                      $IMAGENAME    sh -s <<EOF
@@ -23,3 +27,30 @@ docker run --rm -i -v $SCRIPTDIR:/inputs -v $OUTDIR:/outputs \
 
   chown -R $(id -u):$(id -g) /outputs
 EOF
+################################################################################
+        ;;
+
+    Darwin*)
+################################################################################
+  TMPDIR=/tmp/xj-z3
+  rm -rf $TMPDIR
+  mkdir $TMPDIR
+  cd $TMPDIR
+
+  tar xf $SCRIPTDIR/*.tar.*
+  cd z3-*/
+
+  cmake -B build -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$OUTDIR
+  cmake --build   build -- -j$(nproc --all)
+  cmake --install build
+
+  cd ..
+  rm -rf $TMPDIR
+################################################################################
+        ;;
+
+    *)
+        echo "Running on an unknown OS: ${OS_NAME}"
+        # Handle other operating systems or provide a default action
+        ;;
+esac
