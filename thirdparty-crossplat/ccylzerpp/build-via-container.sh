9fe3f37c7c0efd4d567f0562ad0fb229c2c18906
Ben Karel <bkarel@aarno-labs.com>
2025-10-27 23:36:19 -0400

    Bleh

--- thirdparty-crossplat/ccylzerpp/build-via-container.sh
+++ thirdparty-crossplat/ccylzerpp/build-via-container.sh
@@ -183,7 +183,7 @@ EOF
   #
   git clone https://github.com/brk/cclyzerpp
   cd cclyzerpp
-  git switch tenjin --detach fb0b41aa93b4272018a042b0428a38f74c50100e
+  git switch --detach fb0b41aa93b4272018a042b0428a38f74c50100e
   cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DLLVM_MAJOR_VERSION=${LLVM_MAJOR_VERSION} -DLLVM_PARTIAL_VERSION=${LLVM_PARTIAL_VERSION} -DSOUFFLE_INCLUDE=$TMPDIR/localsouffle/include/ -DBoost_ROOT=$TMPDIR/localboost -DLLVM_DIR=$TMPDIR/xj-llvm/lib/cmake/llvm -DSOUFFLE_USE_LIBFFI=OFF -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DOpenMP_CXX_FLAGS="-fopenmp" -DOpenMP_CXX_INCLUDE_DIR="$TMPDIR/xj-llvm/lib/clang/${LLVM_MAJOR_VERSION}/include/" -DOpenMP_CXX_LIB_NAMES=""
 
   cmake --build build --parallel --target cc2json
