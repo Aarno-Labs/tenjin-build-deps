5541749353e8369abd702d8cdc1f0e6838b2808c
Ben Karel <bkarel@aarno-labs.com>
2026-06-14 23:35:30 -0400

    Update GNU Make 4.4.1 build script

--- thirdparty-linux/gnu-make-4.4.1/build-via-container.sh
+++ thirdparty-linux/gnu-make-4.4.1/build-via-container.sh
@@ -15,9 +15,21 @@ docker run --rm -i -v $SCRIPTDIR:/inputs -v $OUTDIR:/outputs \
   tar xf /inputs/*.tar.*
   cd make-*/
 
-  ./configure --prefix=/outputs
+  # Give an alternate name to /outputs that is easier to spot
+  # in the binary, in case it leaks through.
+  mkdir -p $(dirname $TMPSUBDIR)
+  ln -s /outputs $TMPSUBDIR
+
+  ./configure --prefix=$TMPSUBDIR
+
+  # Disallow $TMPSUBDIR from leaking into the final binary.
+  sed -i.bak 's#-DLOCALEDIR=#-DLOCALEDIR_NOPE=#' Makefile
+  sed -i.bak 's#LOCALEDIR#NULL#' src/main.c
+
   make -j4
   make install
   strip --strip-debug /outputs/bin/make
 
+  # Tenjin doesn't seem to need localization. Save some space.
+  rm -rf /outputs/share/locale
 EOF
