0b9db9fe0e4876f16e6ced8bcea9671890c59043
Ben Karel <bkarel@aarno-labs.com>
2026-06-19 14:38:07 -0400

    find_unsafe2

---
+++ thirdparty-crossplat/find_unsafe2/build-via-container.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+set -eux
+
+CLONE_AND_CD=$(cat << EOM
+  git clone https://github.com/GaloisInc/Tractor-Crisp
+  cd Tractor-Crisp
+  git switch --detach 4d36859307940ac3ab79d6beeb4389bdb6be2039
+EOM
+)
+
+OS_NAME=$(uname -s)
+case "${OS_NAME}" in
+    Linux*)
+################################################################################
+SCRIPTDIR=$(dirname $(realpath "$0"))
+source $SCRIPTDIR/../../thirdparty-linux/common-vars.sh
+
+docker run --rm -i -v $SCRIPTDIR:/inputs -v $OUTDIR:/outputs \
+            --network=host \
+                     $IMAGENAME    bash -s <<EOF
+  set -eux
+  mkdir /tmp/work
+  cd /tmp/work
+
+  $CLONE_AND_CD
+
+  cd tools
+  cargo build --release -p find_unsafe2
+
+  cd find_unsafe2/cargo_subcommands
+  cargo build --release
+  cd ../..  # -> tools
+
+  mkdir -p /outputs/bin
+  ls -l ./target/release/
+  cp ./target/release/{cargo-find-,cargo-check-,find_,check_}unsafe2 /outputs/bin
+
+  chown -R $(id -u):$(id -g) /outputs
+EOF
+################################################################################
+        ;;
+
+    Darwin*)
+################################################################################
+  TMPDIR=/tmp/xj-boost
+  rm -rf $TMPDIR
+  mkdir $TMPDIR
+  cd $TMPDIR 
+  
+  $CLONE_AND_CD
+
+  cd tools
+  cargo build --release -p find_unsafe2
+
+  cd find_unsafe2/cargo_subcommands
+  cargo build --release
+  cd ../.. # -> /REPO/tools
+
+  mkdir -p $OUTDIR/bin/
+  cp ./target/release/{cargo-find-,cargo-check-,find_,check_}unsafe2 $OUTDIR/bin
+  cd ..  # -> /REPO
+
+  cd ..  # -> /
+################################################################################
+        ;;
+
+    *)
+        echo "Running on an unknown OS: ${OS_NAME}"
+        # Handle other operating systems or provide a default action
+        ;;
+esac
+
